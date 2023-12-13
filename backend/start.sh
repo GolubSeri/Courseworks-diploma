cf99964ebf0deddb4eaad4b3b21b3a44adc7459b
GolubSeri <vlad-nikitienko@mail.ru>
2023-12-13 18:39:02 +0300

    fixes

--- backend/start.sh
+++ backend/start.sh
@@ -8,4 +8,4 @@ python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 python manage.py superuserinit
 
-gunicorn Courseworks.wsgi -b 0.0.0.0:8000
+gunicorn Courseworks.wsgi -b 0.0.0.0:8000 --chdir=/app --timeout 90
